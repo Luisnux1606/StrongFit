a014e8e40ed545603856c6917f8996f18614f705
AndreasPC <50256230+AndreasPC@users.noreply.github.com>
2019-05-22 10:03:11 -0500

    Modelo y notas (22-5-2019)
    Porqué está eliminado Entrenamiento y EntrenTiempo

--- src/scripts/baseOracle.sql
+++ src/scripts/baseOracle.sql
@@ -148,9 +148,8 @@ CREATE SEQUENCE iva_id_seq
 /
 
 -- Create sequences CONTABILIDAD -------------------------------------------------
--- Create sequences CONTABILIDAD -------------------------------------------------
 CREATE SEQUENCE TIPO_ASIENTO_SEQ_ID
- INCREMENT BY 1
+ INCREMENT BY 1
  NOMAXVALUE
  NOMINVALUE
  CACHE 20
